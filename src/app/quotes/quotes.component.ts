e84c0cffde34dcd0f6a7bae1f7776c9e467d075e
siddh34 <ssiddharthsutar@gmail.com>
2024-08-09 07:52:20 +0530

    added css to components

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from "../shared/button/button.component";
 
 @Component({
   selector: 'app-quotes',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './quotes.component.html',
   styleUrl: './quotes.component.css'
 })
 export class QuotesComponent {
-
+  quote = "You must be change you want to see in the world";
 }
